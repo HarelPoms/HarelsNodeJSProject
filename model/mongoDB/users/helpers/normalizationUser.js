74ba73368410082bb2d2fe797a703d56eceeabab
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-05-31 09:45:38 +0300

    tweak picture normalization user

--- model/mongoDB/users/helpers/normalizationUser.js
+++ model/mongoDB/users/helpers/normalizationUser.js
@@ -5,8 +5,8 @@ const normalizeUser = (userData) => {
   userData.image = {
     url:
       userData.image.url ||
-      "https://cdn.pixabay.com/photo/2020/04/07/17/01/chicks-5014152_960_720.jpg",
-    alt: userData.image.alt || "yellow fluffy chickens",
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    alt: userData.image.alt || "Default User Profile Picture",
   };
   return {
     ...userData,
