21522d64f5681a0f7ea702ab733f79f26172633d
HarelPoms <95750678+HarelPoms@users.noreply.github.com>
2023-06-25 16:54:00 +0300

    clarified

--- utils/logDateStampMaker.js
+++ utils/logDateStampMaker.js
@@ -4,7 +4,7 @@ const makeLogDateStamp = () => {
     const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     let monthNum = new Date(Date.parse(dateSample[1] +" 1, 2012")).getMonth() + 1;
     let normalizedTime = "GMT" + dateSample[5].slice(0,3) + ":" + dateSample[5].slice(3);
-    return( dateSample2.toLocaleDateString('en-US', {weekday: 'long'}) + " " + dateSample[2]+'/'+monthNum+'/'+dateSample[3]+' '+dateSample[4]+' '+ normalizedTime + " " + timeZone );
+    return( dateSample2.toLocaleDateString('en-US', {weekday: 'long'}) + " " + dateSample[2]+'/' + monthNum + '/' + dateSample[3] + ' ' + dateSample[4] + ' ' + normalizedTime + " " + timeZone );
 }
 
 module.exports = {makeLogDateStamp};
